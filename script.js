d4bee03c5297f178a095176d04d1f2b0b38ad758
Jamesmed1999 <Jamesmed1999@gmail.com>
2021-01-07 20:48:39 -0500

    Created row adder

---
+++ script.js
@@ -0,0 +1,16 @@
+let rowNum = 1
+let colNum = 1
+
+function Row()
+{
+
+    let newRows = document.createElement("tr")
+    let table = document.getElementById("table")
+    for(let i = 0; i<colNum; i++)
+    {
+        let cell = document.createElement("td")
+        newRows.appendChild(cell)
+    }
+    table.appendChild(newRows)
+    rowNum+1
+}
