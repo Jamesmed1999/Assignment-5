1df1f172cd46a2af4af3457e388620a40c8ff3b8
Jamesmed1999 <Jamesmed1999@gmail.com>
2021-01-09 23:11:01 -0500

    creaated a simple color dropdown menu just text atm

--- script.js
+++ script.js
@@ -26,48 +26,18 @@ function Col()
     else{
         let count = 0
         
-        let allRows = document.querySelectorAll("tr");
+        let everyTable = document.querySelectorAll("tr")
         
         for(let i = 0; i<rowNum; i++)
         {
-            let cell = document.createElement("td");
-            allRows[count].appendChild(cell)
+            let cell = document.createElement("td")
+            everyTable[count].appendChild(cell)
             count++
         }
     }
     colNum++;
 }
 
-function goodbyeRow()
-{if(rowNum === 0)
-    {
-        alert("row # = 0")
-    }
-    else{
-    let table = document.getElementById("table")
-    table.deleteRow(rowNum-1)
-    rowNum--;
-}
-}
-
-function goodbyeCol()
-{if(colNum === 0)
-    {
-        alert("col # = 0")
-    }
-    else{
-        let count = 0
-        
-        let allRows = document.querySelectorAll("tr");
-        for(let i = 0; i<rowNum; i++)
-        {
-            allRows[count].removeChild(allRows[count].lastChild);
-
-            count++;
-        }
-        numCol--;
-}
-}
 
 
 
